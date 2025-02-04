a68ffaca2a1454979d49d53362cba10fe6e695de
AtiqurRahmanLabib <atiqurrahmanlabib@gmail.com>
2025-02-05 01:47:55 +0600

    Finish

--- src/Components/Applied-Jobs/AppliedJob.jsx
+++ src/Components/Applied-Jobs/AppliedJob.jsx
@@ -1,22 +1,43 @@
-import bg1 from '../../assets/images/bg1.png'
-import bg2 from '../../assets/images/bg2.png'
-
-const AppliedJob = ({ jobApplied}) => {
-    const { job_title, logo , company_name} = jobApplied;
+import { Link } from 'react-router-dom';
+import iconLocation from '../../assets/icons/location2.png'
+import iconSalary from '../../assets/icons/money.png'
+const AppliedJob = ({ jobApplied }) => {
+    const { job_title, logo, company_name, remote_or_onsite, job_type, salary, location, id } = jobApplied;
     return (
         <div className=''>
             <div className="w-[1120px] h-[200px] mt-[20px] items-center pl-4 flex rounded-[8px] border mx-auto">
-                <div>
-                    <img className='w-[160px] h-[160px]' src={logo} alt="" />
+                <div className='bg-[#F4F4F4] w-[200px] h-[160px] pt-15 pl-5'>
+                    <img className='w-[160px] h-[50px]' src={logo} alt="" />
                 </div>
-                <div>
+                <div className='ml-3'>
                     <h1>{job_title}</h1>
                     <p>{company_name}</p>
-                    <p></p>
+                    <div className='flex gap-2'>
+                        <div className="w-[91px] h-[40px] rounded-[4px] border ">
+                            <h1 className="items-center text-center justify-center mt-1.5 font-extrabold text-[16px] bg-gradient-to-r from-[#7E90FE] to-[#9873FF] bg-clip-text text-transparent">{remote_or_onsite}</h1>
+                        </div>
+                        <div className="w-[91px] h-[40px] rounded-[4px] border ">
+                            <h1 className="items-center text-center justify-center mt-1.5 font-extrabold text-[16px] bg-gradient-to-r from-[#7E90FE] to-[#9873FF] bg-clip-text text-transparent">{job_type}</h1>
+                        </div>
+                        <div>
+                        </div>
+                    </div>
+                    <div className="flex gap-1 items-center mt-2 ">
+                        <div className="flex items-center justify-center gap-1">
+                            <img className="h-[24px] w-[24px]" src={iconLocation} alt="" />
+                            <p className="text-[#757575] text-[20px] font-semibold">{location}</p>
+                        </div>
+                        <div className="flex items-center justify-center gap-1">
+                            <img className="h-[24px] w-[24px]" src={iconSalary} alt="" />
+                            <p className="text-[#757575] text-[20px] font-semibold">{salary}</p>
+                        </div>
+                    </div>
                 </div>
+                <Link to={`/blogs/${id}`} className='ml-[390px]'><button className="text-[#FFFFFF] bg-gradient-to-r from-[#7E90FE] to-[#9873FF] rounded-[4px] w-[157px] h-[49px]  mt-5 text-[18px] font-extrabold ">View Details</button></Link>
             </div>
         </div>
     );
 };
-
 export default AppliedJob;
+
+
--- src/Components/Applied-Jobs/AppliedJobs.jsx
+++ src/Components/Applied-Jobs/AppliedJobs.jsx
@@ -4,7 +4,6 @@ import { getStoredJobApplication } from "../../utils/LocalStorage";
 import AppliedJob from "./AppliedJob";
 import bg1 from '../../assets/images/bg1.png'
 import bg2 from '../../assets/images/bg2.png'
-
 const AppliedJobs = () => {
     const [jobsApplied, setJobsApplied] = useState([]);
     const jobs = useLoaderData();
@@ -12,25 +11,26 @@ const AppliedJobs = () => {
         const storedJobIds = getStoredJobApplication();
         if (jobs.length > 0) {
             // const jobsApllied = jobs.filter(job =>  storedJobIds.includes(job.id)); 
-            const jobsApllied = [];
+            const jobsApplied = [];
             for (const id of storedJobIds) {
                 const job = jobs.find(job => job.id === id);
                 if (job) {
-                    jobsApllied.push(job);
+                    jobsApplied.push(job);
                 }
             }
-            setJobsApplied(jobsApllied);
+            setJobsApplied(jobsApplied);
             // console.log(jobs, storedJobIds, jobsApllied);
         }
     }, [jobs]);
     return (
         <div>
             <div className='flex '>
-                            <img className='absolute' src={bg1} alt="bg1" />
-                            <h1 className='text-center w-full h-[220px] content-center font-extrabold text-[32px] text-[#1A1919]'>Applied Jobs</h1>
-                            <img className='absolute ml-[1222px]' src={bg2} alt="" />
-                        </div>
-                        <div className="mt-[50px] mb-[80px]">
+
+                <img className='absolute' src={bg1} alt="bg1" />
+                <h1 className='text-center w-full h-[220px] content-center font-extrabold text-[32px] text-[#1A1919]'>Applied Jobs</h1>
+                <img className='absolute ml-[1222px]' src={bg2} alt="" />
+            </div>
+            <div className="mt-[50px] mb-[80px]">
                 {
                     jobsApplied.map(jobApplied => <AppliedJob
                         key={jobApplied.id}
@@ -41,5 +41,4 @@ const AppliedJobs = () => {
         </div>
     );
 };
-
 export default AppliedJobs;
